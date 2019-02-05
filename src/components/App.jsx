80e4c06b72cb6e89e844d7f2e9faa10be313fad3
Dylan Ellison <dylancellison@gmail.com>
2019-02-04 23:56:17 -0600

    Revealed open tiles for first and last rows/columns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
   clicked = (i, j) => {
     let grid = this.state.grid.slice();
 
-    revealBlanks(grid,i,j)
+    revealBlanks(grid, i, j);
     grid[i][j].display = "visible";
     this.setState({
       grid
@@ -38,8 +38,13 @@ class App extends Component {
 }
 
 function revealBlanks(grid, i, j) {
+
+  if (grid[i][j].value !== '') {
+    return
+  }
   grid[i][j].checked = true;
-  if (i > 0 && i < 9 && j > 0 && j < 9 && grid[i][j].value === '') {
+  // general case
+  if (i > 0 && i < 9 && j > 0 && j < 9 && grid[i][j].value === "") {
     let perimeter = [
       [i - 1, j - 1],
       [i - 1, j],
@@ -52,11 +57,84 @@ function revealBlanks(grid, i, j) {
     ];
 
     perimeter.forEach(square => {
-
       if (grid[square[0]][square[1]].value === "") {
         grid[square[0]][square[1]].display = "visible";
-        if( !grid[square[0]][square[1]].checked) {
-          revealBlanks(grid, square[0], square[1])
+        if (!grid[square[0]][square[1]].checked) {
+          revealBlanks(grid, square[0], square[1]);
+        }
+      }
+    });
+  }
+  // left column
+  if (j === 0 && i > 0 && i < 9) {
+    const perimeter = [
+      [i - 1, j],
+      [i - 1, j + 1],
+      [i, j + 1],
+      [i + 1, j],
+      [i + 1, j + 1]
+    ];
+    perimeter.forEach(square => {
+      if (grid[square[0]][square[1]].value === "") {
+        grid[square[0]][square[1]].display = "visible";
+        if (!grid[square[0]][square[1]].checked) {
+          revealBlanks(grid, square[0], square[1]);
+        }
+      }
+    });
+  }
+  // right column
+  if (j === 9 && i > 0 && i < 9) {
+    const perimeter = [
+      [i - 1, j],
+      [i - 1, j - 1],
+      [i, j - 1],
+      [i + 1, j],
+      [i + 1, j - 1]
+    ];
+    perimeter.forEach(square => {
+      if (grid[square[0]][square[1]].value === "") {
+        grid[square[0]][square[1]].display = "visible";
+        if (!grid[square[0]][square[1]].checked) {
+          revealBlanks(grid, square[0], square[1]);
+        }
+      }
+    });
+  }
+
+  // top row
+  if (i === 0 && j > 0 && j < 9) {
+    const perimeter = [
+      [i, j - 1],
+      [i, j + 1],
+      [i + 1, j - 1],
+      [i + 1, j],
+      [i + 1, j + 1]
+    ];
+    perimeter.forEach(square => {
+      if (grid[square[0]][square[1]].value === "") {
+        grid[square[0]][square[1]].display = "visible";
+        if (!grid[square[0]][square[1]].checked) {
+          revealBlanks(grid, square[0], square[1]);
+        }
+      }
+    });
+  }
+
+  // bottom row
+  if (i === 9 && j > 0 && j < 9) {
+    const perimeter = [
+      [i, j - 1],
+      [i, j + 1],
+      [i - 1, j - 1],
+      [i - 1, j],
+      [i - 1, j + 1]
+    ];
+    perimeter.forEach(square => {
+      if (grid[square[0]][square[1]].value === "") {
+        grid[square[0]][square[1]].display = "visible";
+        if (!grid[square[0]][square[1]].checked) {
+          revealBlanks(grid, square[0], square[1]);
         }
       }
     });
