82281d3f544b061a66887e1c252c2989e8477de7
Dylan Ellison <dylancellison@gmail.com>
2019-02-05 18:03:49 -0600

    Disable right click

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,26 +3,40 @@ import React, { Component } from "react";
 class Square extends Component {
   state = {
     hidden: true
-  }
+  };
 
   clicked = () => {
-    this.setState({hidden: false})
-  }
+    this.setState({ hidden: false });
+  };
 
   render() {
-    if (this.props.status === 'dead' && this.props.value.value === 'B') {
-      console.log("howdy partner")
-      return <button className="square" style={{background: 'red'}}>{this.props.value.value}</button>
+    if (this.props.status === "dead" && this.props.value.value === "B") {
+      console.log("howdy partner");
+      return (
+        <button className="square" style={{ background: "red" }}>
+          {this.props.value.value}
+        </button>
+      );
     }
 
-    if (this.props.value.display === 'hidden') {
-      return <button onClick={this.props.clicked} className="square" style={{background: '#666'}}></button>;
+    if (this.props.value.display === "hidden") {
+      return (
+        <button
+          onClick={e => {
+            e.preventDefault()
+            console.log("theh clicked button is:", e.button);
+            this.props.clicked();
+          }}
+          onMouseDown={(e) => {console.log(e.button)}}
+          className="square"
+          style={{ background: "#666" }}
+          onContextMenu={(e) => {e.preventDefault()}}
+        />
+      );
     }
 
-    return (
-      <button className="square">{this.props.value.value}</button>
-    )
+    return <button className="square">{this.props.value.value}</button>;
   }
-};
+}
 
 export default Square;
