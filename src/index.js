941bdce491fdd09809341e28bc280ff4e2b266d5
Dylan Ellison <dylancellison@gmail.com>
2019-05-10 20:39:55 -0500

    Hook up redux to use redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import store from './components/store';
 
 import App from "./components/App";
 import reducer from "./reducers";
 import './index.css';
 console.log("the reducer is: ", reducer)
 
-const store = createStore(reducer)
+//const store = createStore(reducer)
 
-console.log("the store is: ", store);
+
 
 render(
   <Provider store={store}>
