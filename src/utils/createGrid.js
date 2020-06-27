e76b609d168f81534f12c5c114cd32417f3baa36
Dylan Ellison <dylancellison@gmail.com>
2020-06-27 06:28:04 -0500

    Apply filter to perimeter to reduce need for edge case checks when creating nearby bomb count

--- src/utils/createGrid.js
+++ src/utils/createGrid.js
@@ -29,72 +29,24 @@ const createGrid = (width, height, bombs) => {
       }
       let minesNearby = 0;
 
-      //grid[i][j] = 0;
-      let perimeter = null;
-      // top left corner case
-      if (i === 0 && j === 0) {
-        perimeter = [grid[i][j + 1], grid[i + 1][j], grid[i + 1][j + 1]];
-        //    console.log("aThe perimter is: ", perimeter)
-      }
-      // top right corner
-      if (i === 0 && j === width - 1) {
-        perimeter = [grid[i][j - 1], grid[i + 1][j], grid[i + 1][j - 1]];
-        //console.log("bThe perimter is: ", perimeter)
-      }
-      // bottom left corner
-      if (i === height - 1 && j === 0) {
-        perimeter = [grid[i - 1][j], grid[i - 1][j + 1], grid[1][j + 1]];
-        //    console.log("cThe perimter is: ", perimeter)
-      }
-      // bottom right corner
-      if (i === height - 1 && j === width - 1) {
-        perimeter = [grid[i][j - 1], grid[i - 1][j - 1], grid[i - 1][j]];
-        //console.log("dThe perimter is: ", perimeter)
-      }
+      let perimeter = [
+        [i - 1, j - 1],
+        [i - 1, j],
+        [i - 1, j + 1],
+        [i, j - 1],
+        [i, j + 1],
+        [i + 1, j - 1],
+        [i + 1, j],
+        [i + 1, j + 1]
+      ];
 
-      // first column
-      if (j === 0 && !perimeter) {
-        perimeter = grid.slice(i - 1, i + 2);
-        perimeter = perimeter.map(row => {
-          return row.slice(0, 2);
-        });
-        perimeter = [].concat.apply([], perimeter);
-      }
-      // last column
-      if (j === width - 1 && !perimeter) {
-        perimeter = grid.slice(i - 1, i + 2);
-        perimeter = perimeter.map(row => {
-          return row.slice(width - 2, width);
-        });
-        perimeter = [].concat.apply([], perimeter);
-      }
+      perimeter = perimeter.filter(per => {
+        return per[0] >= 0 && per[0] < width && per[1] >= 0 && per[1] < height;
+      });
 
-      // top row
-      if (i === 0 && !perimeter) {
-        perimeter = grid.slice(0, 2);
-        perimeter = perimeter.map(row => {
-          return row.slice(j - 1, j + 2);
-        });
-        perimeter = [].concat.apply([], perimeter);
-      }
-      // bottom row
-      if (i === height - 1 && !perimeter) {
-        perimeter = grid.slice(height - 2 , height);
-        perimeter = perimeter.map(row => {
-          return row.slice(j - 1, j + 2);
-        });
-        perimeter = [].concat.apply([], perimeter);
-      }
-
-      // General Case
-      if (!perimeter) {
-        perimeter = grid.slice(i - 1, i + 2);
-        perimeter = perimeter.map(row => {
-          return row.slice(j - 1, j + 2);
-        });
-
-        perimeter = [].concat.apply([], perimeter);
-      }
+      perimeter = perimeter.map(cell => {
+        return grid[cell[0]][cell[1]]
+      })
 
       perimeter.forEach(cell => {
         if (cell === "B") {
