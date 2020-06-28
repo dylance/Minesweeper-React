3963f736166dabcb9cc4fc8a29496c2424281e07
Paul Borawski <paulborawski14@gmail.com>
2020-06-27 21:55:55 -0400

    App.tests completed

--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from '../../store';
 import App from '../App';
 import SelectSize from '../SelectSize';
+import Board from '../Board';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -33,4 +34,12 @@ afterEach(() => {
 
 it('renders one SelectSize Component', () => {
   expect(wrapped.find(SelectSize).length).toEqual(1);
+})
+
+it('renders one h1', () => {
+  expect(wrapped.find('h1').length).toEqual(1);
+})
+
+it('renders one Board component', () => {
+  expect(wrapped.find(Board).length).toEqual(1);
 })
