9971f95b0bbd75898a34cac7b874ac3261751f75
Paul Borawski <paulborawski14@gmail.com>
2020-06-27 21:54:09 -0400

    added beforeEach and afterEach

--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -4,6 +4,7 @@ import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import store from '../../store';
 import App from '../App';
+import SelectSize from '../SelectSize';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -14,4 +15,22 @@ it('renders without crashing', () => {
       div
     );
   ReactDOM.unmountComponentAtNode(div);
+})
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Provider store={store}>
+      <App></App>
+    </Provider>
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+})
+
+it('renders one SelectSize Component', () => {
+  expect(wrapped.find(SelectSize).length).toEqual(1);
 })
