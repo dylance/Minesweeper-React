659ca1b6a49cc2017e544965acc95e0995c3c383
Paul Borawski <paulborawski14@gmail.com>
2020-06-27 22:11:38 -0400

    added tests for board

---
+++ src/components/tests/Board.test.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import store from '../../store';
+import App from '../App';
+import SelectSize from '../SelectSize';
+import Board from '../Board';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <Board
+          grid={[]}
+          height={7}
+          width={7}
+        ></Board>
+      </Provider>,
+      div
+    );
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Provider store={store}>
+      <Board
+        grid={[[{"value":"1","display":"hidden"},{"value":"1","display":"hidden"},{"value":"1","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"}],[{"value":"1","display":"hidden"},{"value":"B","display":"hidden"},{"value":"2","display":"hidden"},{"value":"1","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"}],[{"value":"1","display":"hidden"},{"value":"2","display":"hidden"},{"value":"B","display":"hidden"},{"value":"1","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"}],[{"value":"","display":"hidden"},{"value":"1","display":"hidden"},{"value":"2","display":"hidden"},{"value":"2","display":"hidden"},{"value":"1","display":"hidden"},{"value":"","display":"hidden"},{"value":"","display":"hidden"}],[{"value":"","display":"hidden"},{"value":"1","display":"hidden"},{"value":"2","display":"hidden"},{"value":"B","display":"hidden"},{"value":"2","display":"hidden"},{"value":"1","display":"hidden"},{"value":"","display":"hidden"}],[{"value":"","display":"hidden"},{"value":"1","display":"hidden"},{"value":"B","display":"hidden"},{"value":"3","display":"hidden"},{"value":"B","display":"hidden"},{"value":"1","display":"hidden"},{"value":"","display":"hidden"}],[{"value":"","display":"hidden"},{"value":"1","display":"hidden"},{"value":"1","display":"hidden"},{"value":"2","display":"hidden"},{"value":"1","display":"hidden"},{"value":"1","display":"hidden"},{"value":"","display":"hidden"}]]}
+        height={7}
+        width={7}
+      ></Board>
+    </Provider>
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has props of height equal to the value passed', () => {
+  expect(wrapped.find(Board).prop('height')).toBe(7);
+})
+
+it('has props of width equal to the value passed', () => {
+  expect(wrapped.find(Board).prop('height')).toBe(7);
+})
+
+it('has a grid prop of length equal to array passed', () => {
+  expect(wrapped.find(Board).prop('grid').length).toBe(7);
+})
