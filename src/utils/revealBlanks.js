d139223aa739a0bca6f0968bb14ab10fb24fb660
Dylan Ellison <dylancellison@gmail.com>
2020-07-16 23:03:40 -0500

    Remove unneeded control statement

--- src/utils/revealBlanks.js
+++ src/utils/revealBlanks.js
@@ -9,17 +9,12 @@ export function revealBlanks(grid, i, j, width, height) {
   const perimeter = getPerimeter(i, j, width, height);
   grid[i][j].checked = true;
 
-  perimeter.length === 3
-    ? checkNeighbors(perimeter, grid, width, height)
-    : perimeter.forEach((square) => {
-      if (
-        grid[square[0]][square[1]]
-          && grid[square[0]][square[1]].value === ''
-      ) {
-        grid[square[0]][square[1]].display = 'visible';
-        if (!grid[square[0]][square[1]].checked) {
-          revealBlanks(grid, square[0], square[1], width, height);
-        }
+  perimeter.forEach((square) => {
+    if (grid[square[0]][square[1]] && grid[square[0]][square[1]].value === '') {
+      grid[square[0]][square[1]].display = 'visible';
+      if (!grid[square[0]][square[1]].checked) {
+        revealBlanks(grid, square[0], square[1], width, height);
       }
-    });
+    }
+  });
 }
